5594aa871fd33dab8c81127641d247af96c96712
SatoShun <shun.sato1@gmail.com>
2018-12-13 08:39:35 +0000

    use cloudflare

--- app/src/main/java/com/github/satoshun/example/sample/MainActivity.kt
+++ app/src/main/java/com/github/satoshun/example/sample/MainActivity.kt
@@ -3,14 +3,9 @@ package com.github.satoshun.example.sample
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.github.satoshun.example.sample.databinding.MainActBinding
-import dagger.android.AndroidInjection
-import javax.inject.Inject
 
 class MainActivity : BaseActivity() {
-  @Inject lateinit var resource: UserResource
-
   override fun onCreate(savedInstanceState: Bundle?) {
-    AndroidInjection.inject(this)
     super.onCreate(savedInstanceState)
     val binding = DataBindingUtil.setContentView<MainActBinding>(this, R.layout.main_act)
   }
